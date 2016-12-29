c449dc640061a42a5ac80e27d411730f93b38fe6
wuliubao <ahjxliubao@163.com>
2016-12-30 01:00:53 +0800

    change city

--- scripts/app.js
+++ scripts/app.js
@@ -293,29 +293,29 @@
    * discussion.
    */
   var initialWeatherForecast = {
-    key: '2459115',
+    key: '2475687',
     label: '杭州, 浙江',
     created: '2016-07-22T01:00:00Z',
     channel: {
       astronomy: {
-        sunrise: "5:43 am",
-        sunset: "8:21 pm"
+        sunrise: "8:43 am",
+        sunset: "5:21 pm"
       },
       item: {
         condition: {
           text: "Windy",
-          date: "Thu, 21 Jul 2016 09:00 PM EDT",
-          temp: 56,
+          date: "2016年12月30日, 星期五, 上午10:00",
+          temp: 15,
           code: 24
         },
         forecast: [
-          {code: 44, high: 86, low: 70},
-          {code: 44, high: 94, low: 73},
-          {code: 4, high: 95, low: 78},
-          {code: 24, high: 75, low: 89},
-          {code: 24, high: 89, low: 77},
-          {code: 44, high: 92, low: 79},
-          {code: 44, high: 89, low: 77}
+          {code: 44, high: 16, low: 4},
+          {code: 44, high: 17, low: 4},
+          {code: 4, high: 20, low: 5},
+          {code: 24, high: 13, low: 4},
+          {code: 24, high: 15, low: 3},
+          {code: 44, high: 16, low: 4},
+          {code: 44, high: 15, low: 4}
         ]
       },
       atmosphere: {
